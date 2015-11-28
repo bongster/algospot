0c41fc35c1dad552d4cd89acc75d9e9bb8b95a4e
Daniel Bong <bongster88@gmail.com>
2015-11-28 12:07:55 +0900

    [11560] the_game_of_death pass

--- the_game_of_death.py
+++ the_game_of_death.py
@@ -1,23 +1,29 @@
+'''
+    when I using tail recursion to solve this problem
+    python has a `Runtime error` because python max stack depth is 1000
+'''
 T = int(input())
-
-
-def get_point_number(to_list, to_number, current_idx, try_count):
-    me = to_list[current_idx]
-    if len(to_list) < try_count or current_idx + 1 == me:
-        return 0
-    elif me == to_number:
-        return try_count + 1
-    else:
-        return get_point_number(
-            to_list,
-            to_number,
-            to_list[current_idx] - 1,
-            try_count + 1
-        )
-
 
 for case in range(T):
     number_of_members = int(input())
-    to_list = [int(input()) for me in range(int(number_of_members))]
+    to_list = [int(input()) - 1 for me in range(int(number_of_members))]
 
-    print(get_point_number(to_list, number_of_members, 0, 0))
+    used = [0] * number_of_members
+    next = to_list[0]
+    used[0] = 1
+    count = 0
+
+    while True:
+        if used[next]:
+            count = 0
+            break
+
+        if next == number_of_members - 1:
+            count += 1
+            break
+
+        used[next] = 1
+        count += 1
+        next = to_list[next]
+
+    print count
