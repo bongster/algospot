aaa4c5fccb5175296cc7e293cb5f917afe81c49b
Daniel Bong <bongster88@gmail.com>
2016-01-02 21:48:55 +0900

    [acmicpc] add cache

--- acmicpc/2436_2.cpp
+++ acmicpc/2436_2.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+bool cache[100000001] = {false};
+
 long long getY(long long &a, long long &gcd_value, long long &gcf_value) {
 	return gcf_value / (a / gcd_value);
 }
+
 int main() {
 	long long n, m, length, ans[2] = {100000000, 100000000};
 	scanf("%lld %lld", &n, &m);
@@ -14,11 +17,15 @@ int main() {
 	long long x=0, y=0;
 	for (long long i=1; i < length; i++) {
 		x = n * i;
+		if (cache[x]) {
+			continue;
+		}
 		y = getY(x, n, m);
 		if (x + y < ans[0] + ans[1]) {
 			ans[0] = x;
 			ans[1] = y;
 		}
+		cache[x] = cache[y] = true;
 	}
 
 	printf("%lld %lld\n", ans[0], ans[1]);
